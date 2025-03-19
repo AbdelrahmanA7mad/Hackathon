7eba6215f376a86b8044636e991f7f637b6a05ef
AbdelrahmanA7mad <abdelra7man.ahmad@gmail.com>
2025-03-19 15:05:49 +0200

    add quiz form and fix bugs

---
+++ WaZuF/ViewModels/ExamViewModel.cs
@@ -0,0 +1,15 @@
+﻿using WaZuF.Models;
+
+namespace WaZuF.ViewModels
+{
+    public class ExamViewModel
+    {
+        public int EmployeeId { get; set; } // معرف الموظف الذي يقوم بالامتحان
+        public List<Question> Questions { get; set; } // قائمة الأسئلة الخاصة بالامتحان
+
+        public ExamViewModel()
+        {
+            Questions = new List<Question>(); // تهيئة القائمة لمنع حدوث NullReferenceException
+        }
+    }
+}
